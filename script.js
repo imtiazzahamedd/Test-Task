b93ae7c0a31676fec8995ecefd5ae08d863db6d7
Imtiaz Ahamed <imtiazzahamedd@gmail.com>
2026-02-03 15:23:52 +0600

    done

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function time(){
+function time() {
     const date = new Date();
     document.getElementById("datetime").innerHTML = date.toLocaleString();
 }
 time()
 setInterval(time, 1000);
+
+function send() {
+    event.preventDefault();
+    Swal.fire({
+        title: "Message Sent Successfully!",
+        icon: "success",
+        draggable: true
+    });
+}
